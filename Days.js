42dd64bc77a0f7df1a2a33c795e050d17a7c4c45
weworkcode <90383460+weworkcode@users.noreply.github.com>
2022-03-29 16:13:01 -0700

    changed parentheses to curly brackets on the list

--- Days.js
+++ Days.js
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Days {
     public static void main(String[] args){
         String[] days = new String[7];
-            days[0] = ("Sunday");
-            days[1] = ("Monday");
-            days[2] = ("Tuesday");
-            days[3] = ("Wednesday");
-            days[4] = ("Thursday");
-            days[5] = ("Friday");
-            days[6] = ("Saturday");
+            {
+            days[0] = "Sunday";
+            days[1] = "Monday";
+            days[2] = "Tuesday";
+            days[3] = "Wednesday";
+            days[4] = "Thursday";
+            days[5] = "Friday";
+            days[6] = "Saturday";}
             
             Scanner input = new Scanner(System.in);
                 System.out.println("Input 0 to 6 to spit out the day you choose!");
